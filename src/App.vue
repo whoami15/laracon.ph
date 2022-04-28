d4837354b5e315981777c752e3630e054d79eda9
Daryl Legion <whoamisnowden@gmail.com>
2022-04-28 20:25:18 +0800

    🎉🎉🎉

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<template>
+<div class="flex justify-center items-center h-screen">
+    <img class="h-24 w-24 mr-3" alt="Logo" src="./assets/logo.png" />
+
+    <Home msg="LaraconPH" />
+  </div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+</script>
+
+<template>
+  <h1 class="text-center text-5xl font-bold">
+    {{ msg }}
+  </h1>
+</template>
